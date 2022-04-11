1c943e05fc4a095f9ac0e56753bdb1ffa9f81ba1
Erlon Ressurreição <erlon.ressurreicao@gmail.com>
2022-04-11 17:27:31 -0300

    inclusão de mais funcionalidades

--- src/js/add-product.js
+++ src/js/add-product.js
@@ -1,19 +1,7 @@
-var btnAdd = document.getElementById('add-product');
-
-btnAdd.addEventListener("click", function () {
-    const classTr = 'product'
-    const classTd = 'info-product';
-    const inputform = document.getElementById('input-product').value;
-
-    addProductTable(inputform, classTr, classTd)
-});
-
-function addProductTable(product, classTr, classTd){
+export function addProductTable(product, classTr, classTd) {
     const productTr = buildTr(product, classTr, classTd);
     const table = document.getElementById('table-product');
-    console.log(table);
     table.appendChild(productTr);
-
 }
 
 function buildTr(product, classTr, classTd) {
@@ -27,8 +15,27 @@ function buildTr(product, classTr, classTd) {
 
 function buildTd(dado, classTd) {
     const td = document.createElement("td");
-    td.textContent = dado;
+    const div = document.createElement("div");
+    const input = document.createElement("input");
+    const label = document.createElement("label");
+    // td.textContent = ;
     td.classList.add(classTd);
+    div.classList.add("form-check");
+    input.classList.add("form-check-input");
+    label.classList.add("form-check-label");
+    label.textContent = dado;
+    input.id = "defaultCheck1";
+    input.value = "";
+    input.type = "checkbox";
+    label.for = "defaultCheck1";
+    
+    div.appendChild(input);
+    div.appendChild(label)
+    td.appendChild(div)
+
     return td;
 }
 
+export function cleartInput() {
+    document.getElementById('input-product').value = "";
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,16 @@
-import { header } from './functions.js'
+import { addProductTable, cleartInput } from './add-product.js'
 
-header()
+var btnAdd = document.getElementById('add-product');
+
+btnAdd.addEventListener("click", function () {
+
+    const classTr = 'product'
+    const classTd = 'info-product';
+    const inputform = document.getElementById('input-product').value;
+    if (inputform) {
+        addProductTable(inputform, classTr, classTd);
+    } else {
+        alert('Insira o produto');
+    }
+    cleartInput()
+});
