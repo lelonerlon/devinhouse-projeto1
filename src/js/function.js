dae4faf189ce6a95b5cc42c72c5834d02582e9af
Erlon Ressurreição <erlon.ressurreicao@gmail.com>
2022-04-15 22:51:08 -0300

    Conclusao

--- src/js/function.js
+++ src/js/function.js
@@ -7,28 +7,23 @@ const url_img_delete = 'https://img.icons8.com/ultraviolet/40/000000/delete-sign
 const url_img_payout = 'https://img.icons8.com/fluency/48/000000/money-yours.png';
 
 function saveProduct() {
+    //Obtem o produto a partir do salvamento do produto
     let product = getProduct();
-
+    //Verifica se o input tem dados
     if (product.name) {
+        //Chama a função para incluir no array
         addProduct(product);
+        //Limpa o INPUT do HTML
         document.getElementById('input-product').value = '';
     } else {
         alert('É necessário informar o produto');
     }
-
+    //Monta o HTML após salvar no array
     listTable()
 
 }
 
-function addProduct(product) {
-
-    arr.push(product)
-    localStorage.setItem('data', JSON.stringify(arr))
-    id++;
-
-
-}
-
+//Cria o produto
 function getProduct() {
     let product = {
         id: id,
@@ -40,6 +35,14 @@ function getProduct() {
     return product
 }
 
+//Adiciona o produto no storage
+function addProduct(product) {
+    arr.push(product)
+    localStorage.setItem('data', JSON.stringify(arr))
+    id++;
+}
+
+//Deleta o produto do array
 function deleteProduct(id) {
 
     for (let i = 0; i < arr.length; i++) {
@@ -51,16 +54,12 @@ function deleteProduct(id) {
     }
 }
 
-
-
-
+//Monta a lista de produtos
 function listTable() {
     let tbody = document.getElementById('table-products');
     let totalItem = document.getElementById('total');
     let total = 0;
     tbody.innerText = '';
-
-
 
     for (let i = 0; i < arr.length; i++) {
         let tr = tbody.insertRow();
@@ -70,9 +69,6 @@ function listTable() {
         //Cria a Linha e a coluna
         let span = createElementSpan(arr[i]);
 
-        //Cria o elemento para pagar
-
-
         //Cria o elemento para excluir
         let removeItem = createRemoveItem(arr[i].id);
 
@@ -81,10 +77,11 @@ function listTable() {
         tdName.appendChild(span);
 
         total += Number(arr[i].value);
-        totalItem.innerHTML = ('Total: ' + total).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+        totalItem.innerHTML = (`Total da compra: ${total}`).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
     }
 }
 
+//Cria os elementos dinamicamente, que monta a lista de Produtos
 function createElementSpan(obj) {
     const span = document.createElement('span');
     const div = document.createElement('div');
@@ -93,11 +90,15 @@ function createElementSpan(obj) {
     const s = document.createElement('s');
 
     span.id = `id-${obj.id}`;
+
+    //cria o elemento HTML e define o atributo e valor
     div.classList.add = "form-check";
 
+    //cria o elemento HTML label e define os atributos e valores
     label.classList.add("form-check-label");
     label.setAttribute("for", "flexCheckDefault");
 
+    //cria o o elemento HTML input e define os atributos e valores
     input.classList.add("form-check-input");
     input.value = "#exampleModal";
     input.type = "checkbox";
@@ -107,6 +108,7 @@ function createElementSpan(obj) {
 
     div.appendChild(input);
 
+    //verifica se o Produto está pago para colocar o atachado no item quando é pago
     if (obj.payOut == false) {
         label.textContent = obj.name;
         div.appendChild(label);
@@ -120,15 +122,11 @@ function createElementSpan(obj) {
     }
 
     span.appendChild(div);
-
-
     span.addEventListener('click', () => createModal(obj));
 
     return span
 }
-
-
-
+//Cria dinamicamente a Imagem para remover o produto, 
 function createRemoveItem(elID) {
 
     let removeItem = document.createElement('img');
@@ -138,37 +136,31 @@ function createRemoveItem(elID) {
     return removeItem
 }
 
+//Cria o Modal de acordo com o item selecionado
 function createModal(obj) {
     let title = document.getElementById('exampleModalLabel');
     objAtual = obj;
-    let modalBody = document.querySelector('.modal-body');
-
-    title.innerText = `Item a selecionado: ${obj.name}`;
-
+    title.innerText = `Valor da (o) ${obj.name}`;
 }
 
-function openModal() {
-    const exampleModal = document.getElementById('exampleModal')
+// function openModal() {
+//     const exampleModal = document.getElementById('exampleModal')
 
-    exampleModal.addEventListener('show.bs.modal', function (event) {
-        // Button that triggered the modal
-        var button = event.relatedTarget
+//     exampleModal.addEventListener('show.bs.modal', function (event) {
+//         // Button that triggered the modal
+//         var button = event.relatedTarget
 
-        // Extract info from data-bs-* attributes
-        var recipient = button.getAttribute('data-bs-whatever')
-        // If necessary, you could initiate an AJAX request here
-        // and then do the updating in a callback.
-        //
-        // Update the modal's content.
-        var modalTitle = exampleModal.querySelector('.modal-title')
-        var modalBodyInput = exampleModal.querySelector('.modal-body input')
+   
+//         // var recipient = button.getAttribute('data-bs-whatever')
 
-        modalTitle.textContent = 'New message to ' + recipient
-        modalBodyInput.value = recipient
+//         // var modalTitle = exampleModal.querySelector('.modal-title')
+//         // var modalBodyInput = exampleModal.querySelector('.modal-body input')
 
-    });
-}
 
+//     });
+// }
+
+//Salva o valor do Produto selecionado
 function saveValueItem() {
     let obj = objAtual;
     let valueItem = Number(document.getElementById('valueItem').value.trim())
@@ -186,10 +178,3 @@ function saveValueItem() {
         alert('Para salvar é necessário informar o valor');
     }
 }
-
-
-
-
-
-
-
